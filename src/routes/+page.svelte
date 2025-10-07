06cce17dea61ba44835f678c50b261c8aa07fe37
joshuafolkken <joshuafolkken@gmail.com>
2025-10-07 21:36:31 +0900

    liquid glass #5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import GlassButton from '$lib/GlassButton.svelte';
+	// import GlassSelect from '$lib/GlassSelect.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<GlassButton>うわああああ</GlassButton>
+<GlassButton>やったーーー</GlassButton>
+<GlassButton>しゃべったーーー！！！</GlassButton>
+<!-- <GlassSelect>
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+</GlassSelect> -->
