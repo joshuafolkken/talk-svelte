cf79b4091587cd8648c0ab38e3fd8b8066d7d830
joshuafolkken <joshuafolkken@gmail.com>
2025-10-16 18:32:49 +0900

    check current branch #111

--- scripts/check-commit-message.ts
+++ scripts/check-commit-message.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { execSync } from 'child_process'
-import { readFileSync } from 'fs'
+import { execSync } from 'node:child_process'
+import { readFileSync } from 'node:fs'
 
 interface CommitCheckResult {
 	success: boolean
