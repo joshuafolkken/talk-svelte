b62ef7da4093a79c67d9314ba10495a0be4ad64e
joshuafolkken <joshuafolkken@gmail.com>
2025-11-04 15:41:56 +0900

    Google PageSpeed Insights #281

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import '../app.css'
 	import favicon from '$lib/assets/favicon.svg'
+	import { APP } from '$lib/constants/app'
 
 	let { children } = $props()
 </script>
 
 <svelte:head>
+	<title>{APP.NAME}</title>
+	<meta
+		name="description"
+		content="Listen to English and repeat! Challenge your listening and pronunciation skills in this interactive game."
+	/>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
